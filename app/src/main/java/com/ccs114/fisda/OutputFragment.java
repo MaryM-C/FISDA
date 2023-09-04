7fa88976ab3469fdb300eea46d45ca654f8f9b12
MaryCalamba <calambamarymaee@gmail.com>
2023-09-04 15:56:55 +0800

    #3 #4 : Address the feature to get the main fish image from the Firebase and completed the missing fish information

--- app/src/main/java/com/ccs114/fisda/OutputFragment.java
+++ app/src/main/java/com/ccs114/fisda/OutputFragment.java
@@ -1,42 +1,32 @@
 package com.ccs114.fisda;
 
-import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.ThumbnailUtils;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.databinding.DataBindingUtil;
+import com.ccs114.fisda.databinding.FragmentOutputBinding;
+import com.squareup.picasso.Picasso;
 
 public class OutputFragment extends Fragment {
     FishDataManager fishDataManager = new FishDataManager();
-    private ImageView imageView, refImage;
-    private TextView englishName, comName, localName,edibility;
-    private TextView confidenceView;
-
-    private Button topOne;
-
-    private Button topTwo;
-
-    private Button topThree;
-
-    private ImageButton closeBtn;
-
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_output, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        FragmentOutputBinding bindData =
+                DataBindingUtil.inflate(inflater, R.layout.fragment_output,  container, false);
+
+        View view = bindData.getRoot();
 
         // Retrieve data from the arguments bundle
         Bundle args = getArguments();
@@ -45,130 +35,46 @@ public class OutputFragment extends Fragment {
             String[] topFishSpecies = args.getStringArray("topFishSpecies");
             String[] topConfidences = args.getStringArray("topConfidences");
 
-            // Find the ImageView and TextView by their IDs
-            imageView = (ImageView) view.findViewById(R.id.imgInputFish);
-            englishName = view.findViewById(R.id.txtEngName);
-            comName = view.findViewById(R.id.txtComName);
-            localName = view.findViewById(R.id.txtLocName);
-            edibility = view.findViewById(R.id.txtEdibility);
-            confidenceView = view.findViewById(R.id.lblConfidence);
-            topOne = view.findViewById(R.id.btnResultOne);
-            topTwo = view.findViewById(R.id.btnResultTwo);
-            topThree = view.findViewById(R.id.btnResultThree);
-            closeBtn = view.findViewById(R.id.btnBack);
-            refImage = view.findViewById(R.id.imgFishSpecies);
-
-
             //button
-            topOne.setEnabled(false);
+            bindData.btnResultOne.setEnabled(false);
             //Image from the user
-            displayImage(imageView, refImage, args);
+            displayImage(bindData.imgInputFish, bindData.imgFishSpecies, args);
 
             //TODO Add the image from the Firebase here
 
 
+            displayFishInfo(bindData, topFishSpecies[0], topConfidences[0]);
+//
 
-            fishDataManager.getFishData(topFishSpecies[0], new FishDataManager.FishDataListener() {
-                public void onFishDataLoaded(Fish fish) {
-                    comName.setText(topFishSpecies[0]);
-                    confidenceView.setText(topConfidences[0] + "%");
+            bindData.btnResultOne.setOnClickListener(view1 -> {
+                displayFishInfo(bindData, topFishSpecies[0], topConfidences[0]);
+                bindData.btnResultOne.setEnabled(false);
 
-                    englishName.setText(fish.getEnglishName());
-                    localName.setText(fish.getLocalName());
-                    edibility.setText(fish.getEdibility());
+                bindData.btnResultTwo.setEnabled(true);
+                bindData.btnResultThree.setEnabled(true);
 
+            });
+            bindData.btnResultTwo.setOnClickListener(view12 -> {
+               displayFishInfo(bindData, topFishSpecies[1], topConfidences[1]);
+                bindData.btnResultTwo.setEnabled(false);
 
-                }
-                public void onFishDataNotFound() {
-                    Toast.makeText(getContext(), "Fish data not found.", Toast.LENGTH_SHORT).show();
-                }
-                @Override
-                public void onFishDataError(String errorMessage) {
-                    Toast.makeText(getContext(), "Fish data not found." + errorMessage, Toast.LENGTH_SHORT).show();
-                }
+                bindData.btnResultOne.setEnabled(true);
+                bindData.btnResultThree.setEnabled(true);
+            });
+            bindData.btnResultThree.setOnClickListener(view13 -> {
+                displayFishInfo(bindData, topFishSpecies[2], topConfidences[2]);
+                bindData.btnResultThree.setEnabled(false);
+
+                bindData.btnResultOne.setEnabled(true);
+                bindData.btnResultTwo.setEnabled(true);
             });
 
-            topOne.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    fishDataManager.getFishData(topFishSpecies[0], new FishDataManager.FishDataListener() {
-                        public void onFishDataLoaded(Fish fish) {
-                            comName.setText(topFishSpecies[0]);
-                            confidenceView.setText(topConfidences[0] + "%");
-
-                            englishName.setText(fish.getEnglishName());
-                            localName.setText(fish.getLocalName());
-                            edibility.setText(fish.getEdibility());
-                        }
-                        public void onFishDataNotFound() {
-                            Toast.makeText(getContext(), "Fish data not found.", Toast.LENGTH_SHORT).show();
-                        }
-                        @Override
-                        public void onFishDataError(String errorMessage) {
-                            Toast.makeText(getContext(), "Fish data not found." + errorMessage, Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                    
-                }
-            });
-            topTwo.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    fishDataManager.getFishData(topFishSpecies[1], new FishDataManager.FishDataListener() {
-                        public void onFishDataLoaded(Fish fish) {
-                            comName.setText(topFishSpecies[1]);
-                            confidenceView.setText(topConfidences[1] + "%");
-
-                            englishName.setText(fish.getEnglishName());
-                            localName.setText(fish.getLocalName());
-                            edibility.setText(fish.getEdibility());
-                            topOne.setEnabled(true);
-                        }
-                        public void onFishDataNotFound() {
-                            Toast.makeText(getContext(), "Fish data not found.", Toast.LENGTH_SHORT).show();
-                        }
-                        @Override
-                        public void onFishDataError(String errorMessage) {
-                            Toast.makeText(getContext(), "Fish data not found." + errorMessage, Toast.LENGTH_SHORT).show();
-                        }
-                    });
-
-
-                }
-            });
-            topThree.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    fishDataManager.getFishData(topFishSpecies[2], new FishDataManager.FishDataListener() {
-                        public void onFishDataLoaded(Fish fish) {
-                            comName.setText(topFishSpecies[2]);
-                            confidenceView.setText(topConfidences[2] + "%");
-
-                            englishName.setText(fish.getEnglishName());
-                            localName.setText(fish.getLocalName());
-                            edibility.setText(fish.getEdibility());
-                            topOne.setEnabled(true);
-                        }
-                        public void onFishDataNotFound() {
-                            Toast.makeText(getContext(), "Fish data not found.", Toast.LENGTH_SHORT).show();
-                        }
-                        @Override
-                        public void onFishDataError(String errorMessage) {
-                            Toast.makeText(getContext(), "Fish data not found." + errorMessage, Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                }
-            });
-
-            closeBtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    CaptureFragment captureFragment = new CaptureFragment();
-                    FragmentManager manager = requireActivity().getSupportFragmentManager();
-                    FragmentTransaction transaction = manager.beginTransaction();
-                    transaction.replace(R.id.container, captureFragment);
-                    transaction.commit();
-                }
+            bindData.btnBack.setOnClickListener(view14 -> {
+                CaptureFragment captureFragment = new CaptureFragment();
+                FragmentManager manager = requireActivity().getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+                transaction.replace(R.id.container, captureFragment);
+                transaction.commit();
             });
         }
 
@@ -176,21 +82,71 @@ public class OutputFragment extends Fragment {
         return view;
     }
 
+    private void displayFishInfo(FragmentOutputBinding bindData, String fishName, String confidence) {
+        fishDataManager.getFishData(fishName, new FishDataManager.FishDataListener() {
+            public void onFishDataLoaded(Fish fish) {
+
+                bindData.setConfidence(confidence + "%");
+                bindData.scrlInfo.setEnglishName(fishName);
+
+                displayBasicInfo(bindData, fish);
+                displayTaxonomy(bindData, fish);
+
+                bindData.scrlInfo.setShortDescription(fish.getShortDescription());
+
+                displayBioInfo(bindData, fish);
+
+                Picasso.get().load(fish.getMainImage()).into(bindData.imgFishSpecies);
+
+            }
+            public void onFishDataNotFound() {
+                Toast.makeText(getContext(), "Fish data not found.", Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onFishDataError(String errorMessage) {
+                Toast.makeText(getContext(), "Fish data not found." + errorMessage, Toast.LENGTH_SHORT).show();
+            }
+
+            private void displayBasicInfo(FragmentOutputBinding bindData, Fish fish) {
+                bindData.scrlInfo.setLocalName(fish.getLocalName());
+                bindData.scrlInfo.setCommonName(fish.getCommonName());
+                bindData.scrlInfo.setEdibility(fish.getEdibility());
+                bindData.scrlInfo.setCategory(fish.getCategory());
+            }
+
+            private void displayTaxonomy(FragmentOutputBinding bindData, Fish fish) {
+                bindData.scrlInfo.setVarClass(fish.getTClass());
+                bindData.scrlInfo.setOrder(fish.getOrder());
+                bindData.scrlInfo.setFamily(fish.getFamily());
+                bindData.scrlInfo.setGenus(fish.getGenus());
+                bindData.scrlInfo.setSciName(fish.getScientificName());
+            }
+
+            private void displayBioInfo(FragmentOutputBinding bindData, Fish fish) {
+                bindData.scrlInfo.setMaxLength(fish.getMaxLength());
+                bindData.scrlInfo.setMaxWeight(fish.getMaxWeight());
+                bindData.scrlInfo.setSize(fish.getSize());
+                bindData.scrlInfo.setEnvironment(fish.getEnvironment());
+                bindData.scrlInfo.setTemperature(fish.getTemperature());
+                bindData.scrlInfo.setDiet(fish.getDiet());
+            }
+        });
+    }
+
     private void displayImage(ImageView imageView, ImageView refImage,  Bundle args) {
         byte[] byteArray = args.getByteArray("imagebytes");
         if(byteArray != null) {
             Bitmap image = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+            image = ThumbnailUtils.extractThumbnail(image, 150, 90);
             imageView.setImageBitmap(image);
             //TODO
             /*delete if the image from the Firebase is added*/
-            Bitmap thumbnail = ThumbnailUtils.extractThumbnail(image, 370, 200);
-            refImage.setImageBitmap(thumbnail);
+//            Bitmap thumbnail = ThumbnailUtils.extractThumbnail(image, 370, 200);
+//            refImage.setImageBitmap(thumbnail);
+
+
         }
     }
-
-
-
-
 
 
 }
