4c0eaa5d6badefabb9a136e1edaaf7b2789e3641
MaryCalamba <calambamarymaee@gmail.com>
2023-11-28 01:41:00 +0800

    Renamed to follow camel case

--- app/src/main/java/com/ccs114/fisda/Offline.java
+++ app/src/main/java/com/ccs114/fisda/Offline.java
@@ -6,7 +6,7 @@ import com.google.firebase.database.FirebaseDatabase;
 
 
 
-public class offline extends Application {
+public class Offline extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
