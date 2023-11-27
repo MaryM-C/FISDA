2b9dec76e4cad723b3b0a9e53aaa1396da0f1535
MaryCalamba <calambamarymaee@gmail.com>
2023-11-28 02:06:47 +0800

    Modified the unit tests to accomodate the changes of Bundle contents

--- app/src/test/java/com/ccs114/fisda/CaptureFragmentTest.java
+++ app/src/test/java/com/ccs114/fisda/CaptureFragmentTest.java
@@ -15,32 +15,32 @@ public class CaptureFragmentTest {
 
     static Stream<Arguments> testDataForFishInputInfo() {
         return Stream.of(
-                Arguments.of(new byte[]{1, 2, 3}, new String[]{"Trout", "Salmon", "Bass"},
+                Arguments.of("image1.png", new String[]{"Trout", "Salmon", "Bass"},
                         new String[]{"0.05", "0.10", "0.20"}, "path1", "file1", new OutputHandler()),
 
-                Arguments.of(new byte[]{4, 5, 6}, new String[]{"Tuna", "Swordfish", "Mackerel"},
+                Arguments.of("image2.png", new String[]{"Tuna", "Swordfish", "Mackerel"},
                         new String[]{"0.51", "0.23", "0.80"}, "path2", "file2", new OutputHandler()),
 
-                Arguments.of(new byte[]{7, 8, 9}, new String[]{"Carp", "Catfish", "Perch"},
+                Arguments.of("image3.png", new String[]{"Carp", "Catfish", "Perch"},
                         new String[]{"5.0", "4.0", "6.0"}, "path3", "file3", new OutputHandler())
         );
     }
 
     @ParameterizedTest
     @MethodSource("testDataForFishInputInfo")
-    public void fishInputInfo_shouldReturnCorrectArgs(byte[] fishByteArray, String[] topFishSpecies, String[] topConfidences,
+    public void fishInputInfo_shouldReturnCorrectArgs(String uri, String[] topFishSpecies, String[] topConfidences,
                                                       String imagepath, String imageFileName, OutputHandler handler) {
 
         Bundle expected = new Bundle();
 
-        expected.putByteArray("imagebytes", fishByteArray);
+        expected.putString("uri", uri);
         expected.putStringArray("topFishSpecies", topFishSpecies);
         expected.putStringArray("topConfidences", topConfidences);
         expected.putString("imagepath", imagepath);
         expected.putString("filename", imageFileName);
 
         handler.setConfidence(new float[3]);
-        Bundle result = captureFragment.fishInputInfo(fishByteArray, topFishSpecies, topConfidences,
+        Bundle result = captureFragment.fishInputInfo(uri, topFishSpecies, topConfidences,
                 imagepath, imageFileName, handler);
 
         assertThat(result).isNotNull();
