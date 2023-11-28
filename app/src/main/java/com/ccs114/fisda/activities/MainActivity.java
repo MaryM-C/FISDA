7701a98694dd2fff5321e8e852d7e05df22d7df5
MaryCalamba <calambamarymaee@gmail.com>
2023-11-29 00:16:31 +0800

    Created 'activities' directory for  activity classes

---
+++ app/src/main/java/com/ccs114/fisda/activities/MainActivity.java
@@ -0,0 +1,59 @@
+package com.ccs114.fisda.activities;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+
+
+import com.ccs114.fisda.R;
+import com.ccs114.fisda.fragments.CaptureFragment;
+import com.ccs114.fisda.fragments.CollectionFragment;
+import com.ccs114.fisda.fragments.HomeFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    HomeFragment homeFragment = new HomeFragment();
+    CaptureFragment captureFragment = new CaptureFragment();
+    CollectionFragment collectionsFragment = new CollectionFragment();
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView  = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.capture);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,captureFragment).commit();
+
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if(id == R.id.home){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+                    return true;
+                } else if (id == R.id.capture) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container,captureFragment).commit();
+                    return true;
+                }else{
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container,collectionsFragment).commit();
+                    return true;
+                }
+            }
+        });
+    }
+
+
+
+
+
+
+
+}
